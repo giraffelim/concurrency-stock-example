72f78c5b3bd51ab889daa187db6161c472512645
giraffelim <taeyang7704@naver.com>
2022-09-10 15:05:46 +0900

    config: init project setting

---
+++ src/main/java/com/giraffelim/stock/ConcurrencyStockExampleApplication.java
@@ -0,0 +1,13 @@
+package com.giraffelim.stock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrencyStockExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrencyStockExampleApplication.class, args);
+    }
+
+}
