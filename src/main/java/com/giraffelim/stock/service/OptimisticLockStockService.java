d569d6114b7164ab131a49ec07385d8cd88c3cbe
giraffelim <taeyang7704@naver.com>
2022-09-11 01:06:37 +0900

    feature: OptimisticLock 구현

---
+++ src/main/java/com/giraffelim/stock/service/OptimisticLockStockService.java
@@ -0,0 +1,23 @@
+package com.giraffelim.stock.service;
+
+import com.giraffelim.stock.domain.Stock;
+import com.giraffelim.stock.repository.StockRepository;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class OptimisticLockStockService {
+
+    private final StockRepository stockRepository;
+
+    public OptimisticLockStockService(StockRepository stockRepository) {
+        this.stockRepository = stockRepository;
+    }
+
+    @Transactional
+    public void decrease(Long id, Long quantity) {
+        Stock stock = stockRepository.findByIdWithOptimisticLock(id);
+        stock.decrease(quantity);
+    }
+
+}
